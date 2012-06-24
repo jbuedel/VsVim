f88f5b17e0fe51fd7023257924f37a5641c7b779
Jared Parsons <jaredpparsons@gmail.com>
2012-06-23 17:19:35 -0700

    Enabled strong name signing
    Strong name signing is required in order to be a Visual Studio package.
    Can't fully enable it here because of build errors related to
    VsEditor.Interop.  Commiting so I can sync up with that fix and then
    resolve the strong name issues

--- VimCore/AssemblyInfo.fs
+++ VimCore/AssemblyInfo.fs
@@ -5,7 +5,7 @@ namespace Vim
 
 open System.Runtime.CompilerServices
 
-[<assembly:InternalsVisibleTo("Vim.Core.UnitTest")>]
-[<assembly:InternalsVisibleTo("Vim.UnitTest.Utils")>]
-[<assembly:InternalsVisibleTo("DynamicProxyGenAssembly2")>] // Moq
+[<assembly:InternalsVisibleTo("Vim.Core.UnitTest, PublicKeyToken=d268dfe9ddbfede3")>]
+// Moq assembly
+[<assembly:InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKeyToken=a621a9e7e5c32e69")>]
 do()
--- VsVimShared/Properties/AssemblyInfo.cs
+++ VsVimShared/Properties/AssemblyInfo.cs
@@ -34,5 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("VsVim.Shared.UnitTest")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Moq
+[assembly: InternalsVisibleTo("VsVim.Shared.UnitTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f3cf40dc096cdd74d402819f186adbc653981040273f9d47d4f006f33b94d606898dbd4c6e964e997371e1427fdb049f3366787fec84fb1f466db63917379e37db4a6d65d88df63f80d124e35c50cdb7c7a1822e080c7cbbc9699a4be8a22c093f5f7d47b8920d5c848074ed60299054358d796c740aa98763b933d9ced30696")]
+
+// Moq
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2,PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")] 
