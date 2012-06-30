528cbe7fea51537edbed82cb5bf3971810da8259
Jared Parsons <jaredpparsons@gmail.com>
2012-06-30 07:29:36 -0700

    Tool window showing
    Lots of bugs in the tool window that I want to handle as a separate
    change.  It is properly displaying now without error

--- VsVim/ToolWindow.cs
+++ VsVim/ToolWindow.cs
@@ -1,15 +1,67 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
 
 namespace VsVim
 {
-    [Guid("4869721e-9926-4ead-8dea-10c351f08358")]
+    [Guid(GuidList.VsVimToolWindowGuidString)]
     public sealed class ToolWindow : ToolWindowPane
     {
+        private readonly DockPanel _dockPanel;
+        private ITextView _textView;
+
         public ToolWindow()
         {
+            _dockPanel = new DockPanel();
 
+            // Set the window title reading it from the resources.
+            Caption = Resources.ToolWindowTitle;
+
+            // Set the image that will appear on the tab of the window frame
+            // when docked with an other window
+            // The resource ID correspond to the one defined in the resx file
+            // while the Index is the offset in the bitmap strip. Each image in
+            // the strip being 16x16.
+            BitmapResourceID = 301;
+            BitmapIndex = 1;
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on 
+            // the object returned by the Content property.
+
+            Content = _dockPanel;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            var serviceProvider = this;
+            var componentModel = serviceProvider.GetService<SComponentModel, IComponentModel>();
+
+            // First create the ITextBuffer with a content type of text.  One day we may want to switch it
+            // to use a Vim specific content type but for now just stick with text.  This is a general purpose
+            // window
+            var textBufferFactoryService = componentModel.DefaultExportProvider.GetExport<ITextBufferFactoryService>().Value;
+            var textBuffer = textBufferFactoryService.CreateTextBuffer(textBufferFactoryService.TextContentType);
+            textBuffer.Insert(0, "VsVim Scratch Pad");
+
+            // Now create the containing view.  Want a simple document here but no outlining 
+            var textEditorFactoryService = componentModel.DefaultExportProvider.GetExport<ITextEditorFactoryService>().Value;
+            var textViewRoleSet = textEditorFactoryService.CreateTextViewRoleSet(
+                PredefinedTextViewRoles.PrimaryDocument,
+                PredefinedTextViewRoles.Document,
+                PredefinedTextViewRoles.Editable,
+                PredefinedTextViewRoles.Interactive);
+            var textView = textEditorFactoryService.CreateTextView(textBuffer, textViewRoleSet);
+
+            // Now update the display to be the document
+            var textViewHost = textEditorFactoryService.CreateTextViewHost(textView, setFocus: false);
+            _dockPanel.Children.Add(textViewHost.HostControl);
         }
     }
 }
--- VsVim/VsVimPackage.cs
+++ VsVim/VsVimPackage.cs
@@ -1,5 +1,9 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace VsVim
 {
@@ -31,9 +35,43 @@ namespace VsVim
 
         }
 
+        /// <summary>
+        /// This function is called when the user clicks the menu item that shows the 
+        /// tool window. See the Initialize method to see how the menu item is associated to 
+        /// this function using the OleMenuCommandService service and the MenuCommand class.
+        /// </summary>
+        private void ShowToolWindow(object sender, EventArgs e)
+        {
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = this.FindToolWindow(typeof(ToolWindow), 0, true);
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException(Resources.CanNotCreateWindow);
+            }
+
+            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initilaization code that rely on services provided by VisualStudio.
+        /// </summary>
         protected override void Initialize()
         {
             base.Initialize();
+
+            // Add our command handlers for menu (commands must exist in the .vsct file)
+            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
+            {
+                // Create the command for the tool window
+                CommandID toolwndCommandID = new CommandID(GuidList.VsVimCommandSetGuid, (int)PackageCommandIds.cmdidMyTool);
+                MenuCommand menuToolWin = new MenuCommand(ShowToolWindow, toolwndCommandID);
+                mcs.AddCommand(menuToolWin);
+            }
         }
     }
 }
